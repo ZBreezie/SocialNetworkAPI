6c494133a4cbcfa942eeba832be6ce164ba6269f
ZBreezie <zpmc55@yahoo.com>
2022-01-08 18:35:26 -0500

    added routes/controllers/thought reaction orderswap

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,36 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
@@ -34,39 +64,9 @@ const ThoughtSchema = new Schema(
 
 const Thought = model('Thought', ThoughtSchema);
 
-// get total count of friends on retrieval
+// gets the length of the thoughts reactions array field
 ThoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
-
-const ReactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-        }
-    },
-    {
-        toJSON: {
-            getters: true
-        }
-    }
-)
 
 module.exports = Thought;
