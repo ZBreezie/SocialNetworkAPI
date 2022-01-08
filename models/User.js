4467fb24b29b0006b627d75d9992d15054885211
ZBreezie <zpmc55@yahoo.com>
2022-01-07 19:18:39 -0500

    added thought and user models/momentjs/ and server files

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+const User = model('User', UserSchema);
+
+// gets the length of the user's friends array field
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+
+// export the User model
+module.exports = User;
